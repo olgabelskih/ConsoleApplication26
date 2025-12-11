ca5822c709857ca5be76ab497c4fd87497520bb5
243530 <243530@KB37-309A-C09>
2025-12-11 13:24:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication26/ConsoleApplication26.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "libery.h"
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    bibleoteka lib;
+    lib.my_bibleoteka();
+    cout << endl;
+    return 0;
+}
